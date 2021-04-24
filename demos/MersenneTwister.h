d20b96bbe157d9ac9b6c44b58a233d4abaff7bc4
Bruno Manganelli <bruno.manga95@gmail.com>
2021-04-24 13:52:10 +0200

    Add clang-format and format files

--- demos/MersenneTwister.h
+++ demos/MersenneTwister.h
@@ -26,112 +26,103 @@
 */
 
 //! Mersenne-Twister class.
-class MersenneTwister
-{
-public:
-    //! Constructor.
-    MersenneTwister()
-    {
-        // Get a hardware random number and seed the generator.
-        seed = std::random_device{}();
-        generator.seed(seed);
+class MersenneTwister {
+ public:
+  //! Constructor.
+  MersenneTwister()
+  {
+    // Get a hardware random number and seed the generator.
+    seed = std::random_device{}();
+    generator.seed(seed);
 
-        /* Note that seeding the generator with a single 32-bit integer
-           only allows for 2^32 initial states.
+    /* Note that seeding the generator with a single 32-bit integer
+       only allows for 2^32 initial states.
 
-          For a better RNG, consider using the Permuted Congruential Generator
+      For a better RNG, consider using the Permuted Congruential Generator
 
-            http://www.pcg-random.org
+        http://www.pcg-random.org
 
-          For details on the pitfalls of seeding the C++11 mt19937 generator,
-          see
+      For details on the pitfalls of seeding the C++11 mt19937 generator,
+      see
 
-            http://www.pcg-random.org/posts/cpp-seeding-surprises.html
+        http://www.pcg-random.org/posts/cpp-seeding-surprises.html
 
-          To enable good seeding with mt19937, consider using randutils.hpp
+      To enable good seeding with mt19937, consider using randutils.hpp
 
-            https://gist.github.com/imneme/540829265469e673d045
-         */
-    }
-
-    //! Overloaded () operator.
-    /*! \return A uniform random double in range [0-1]. */
-    double operator()()
-    {
-        return default_uniform_real_distribution(generator);
-    }
-
-    //! Generate a random integer between min and max (inclusive).
-    /*! \param min
-            The minium of the range.
-
-        \param max
-            The maxium of the range.
-
-        \return
-            The uniform random integer.
+        https://gist.github.com/imneme/540829265469e673d045
      */
-    int integer(int min, int max)
-    {
-        return std::uniform_int_distribution<int>{min, max}(generator);
-    }
+  }
 
-    //! Generate a random number from a normal distribution with
-    /*! zero mean and unit standard deviation.
-        \return
-            A random number drawn from the normal distribution.
-     */
-    double normal()
-    {
-        return default_normal_distribution(generator);
-    }
+  //! Overloaded () operator.
+  /*! \return A uniform random double in range [0-1]. */
+  double operator()() { return default_uniform_real_distribution(generator); }
 
-    //! Generate a random number from a normal distribution.
-    /*! \param mean
-            The mean of the the normal distribution.
+  //! Generate a random integer between min and max (inclusive).
+  /*! \param min
+          The minium of the range.
 
-        \param stdDev
-            The standard deviation of the normal distribution.
+      \param max
+          The maxium of the range.
 
-        \return
-            A random number drawn from the normal distribution.
-     */
-    double normal(double mean, double stdDev)
-    {
-        return std::normal_distribution<double>{mean, stdDev}(generator);
-    }
+      \return
+          The uniform random integer.
+   */
+  int integer(int min, int max)
+  {
+    return std::uniform_int_distribution<int>{min, max}(generator);
+  }
 
-    //! Get the random number generator seed.
-    /*! \return seed
-            The generator seed.
-     */
-    unsigned int getSeed()
-    {
-        return seed;
-    }
+  //! Generate a random number from a normal distribution with
+  /*! zero mean and unit standard deviation.
+      \return
+          A random number drawn from the normal distribution.
+   */
+  double normal() { return default_normal_distribution(generator); }
 
-    //! Seed the random number generator.
-    /*! \param seed_
-            The new seed.
-     */
-    void setSeed(unsigned int seed_)
-    {
-        seed = seed_;
-        generator.seed(seed);
-    }
+  //! Generate a random number from a normal distribution.
+  /*! \param mean
+          The mean of the the normal distribution.
 
-private:
-    /// The Mersenne-Twister generator.
-    std::mt19937 generator;
+      \param stdDev
+          The standard deviation of the normal distribution.
 
-    /// Default uniform_real distribution [0-1].
-    std::uniform_real_distribution<double> default_uniform_real_distribution{0.0, 1.0};
+      \return
+          A random number drawn from the normal distribution.
+   */
+  double normal(double mean, double stdDev)
+  {
+    return std::normal_distribution<double>{mean, stdDev}(generator);
+  }
 
-    /// Default normal distribution with zero mean and unit standard deviation.
-    std::normal_distribution<double> default_normal_distribution{0.0, 1.0};
+  //! Get the random number generator seed.
+  /*! \return seed
+          The generator seed.
+   */
+  unsigned int getSeed() { return seed; }
 
-    /// The random number seed.
-    unsigned int seed;
+  //! Seed the random number generator.
+  /*! \param seed_
+          The new seed.
+   */
+  void setSeed(unsigned int seed_)
+  {
+    seed = seed_;
+    generator.seed(seed);
+  }
+
+ private:
+  /// The Mersenne-Twister generator.
+  std::mt19937 generator;
+
+  /// Default uniform_real distribution [0-1].
+  std::uniform_real_distribution<double> default_uniform_real_distribution{0.0,
+                                                                           1.0};
+
+  /// Default normal distribution with zero mean and unit standard deviation.
+  std::normal_distribution<double> default_normal_distribution{0.0, 1.0};
+
+  /// The random number seed.
+  unsigned int seed;
 };
 
-#endif  /* _MERSENNETWISTER_H */
+#endif /* _MERSENNETWISTER_H */
